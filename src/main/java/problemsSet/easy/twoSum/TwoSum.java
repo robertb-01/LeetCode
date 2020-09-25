c9db8880b0e7034472a1442de8a291d03354be57
robertb-01 <robert.bosoiu@yahoo.com>
2020-09-25 22:38:32 +0300

    + gitignore

---
+++ src/main/java/problemsSet/easy/twoSum/TwoSum.java
@@ -0,0 +1,13 @@
+package problemsSet.easy.twoSum;
+
+public class TwoSum {
+    public static void main(String[] args) {
+        System.out.println(twoSumV1(1,2));
+    }
+
+    static int twoSumV1(int a, int b) {
+        return a + b;
+    }
+}
+
+
